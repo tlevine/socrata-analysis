7fd93725c10e05a8bd4362a025eb2a92ffaeadfc
Thomas Levine <occurrence@thomaslevine.com>
2013-06-26 17:52:04 -0400

    encapsulate stuff in run.py

--- run.py
+++ run.py
@@ -16,50 +16,51 @@ def rows():
             if row != None:
                 yield row
 
-# metadata = socrata.concat_to_array(rows())
+def build_ndarray():
+    return socrata.concat_to_array(rows())
 
+def build_csv():
+    class UnicodeDictWriter:
+        """
+        A CSV writer which will write rows to CSV file "f",
+        which is encoded in the given encoding.
+        """
 
-class UnicodeDictWriter:
-    """
-    A CSV writer which will write rows to CSV file "f",
-    which is encoded in the given encoding.
-    """
+        def __init__(self, f, fieldnames, dialect=csv.excel, encoding="utf-8", **kwds):
+            # Redirect output to a queue
+            self.queue = cStringIO.StringIO()
+            self.writer = csv.DictWriter(self.queue, fieldnames, dialect=dialect, **kwds)
+            self.stream = f
+            self.encoder = codecs.getincrementalencoder(encoding)()
 
-    def __init__(self, f, fieldnames, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.DictWriter(self.queue, fieldnames, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
+        @staticmethod
+        def encode_if_text(value):
+            if isinstance(value, basestring):
+                return value.encode('utf-8')
+            else:
+                return value
 
-    @staticmethod
-    def encode_if_text(value):
-        if isinstance(value, basestring):
-            return value.encode('utf-8')
-        else:
-            return value
+        def writerow(self, row):
+            self.writer.writerow(OrderedDict(
+                [(UnicodeDictWriter.encode_if_text(k), UnicodeDictWriter.encode_if_text(v)) for k,v in row.items()]
+            ))
+            # Fetch UTF-8 output from the queue ...
+            data = self.queue.getvalue()
+            data = data.decode("utf-8")
+            # ... and reencode it into the target encoding
+            data = self.encoder.encode(data)
+            # write to the target stream
+            self.stream.write(data)
+            # empty queue
+            self.queue.truncate(0)
 
-    def writerow(self, row):
-        self.writer.writerow(OrderedDict(
-            [(UnicodeDictWriter.encode_if_text(k), UnicodeDictWriter.encode_if_text(v)) for k,v in row.items()]
-        ))
-        # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
-        # empty queue
-        self.queue.truncate(0)
+        def writerows(self, rows):
+            for row in rows:
+                self.writerow(row)
 
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
-
-f = open('socrata.csv', 'w')
-w = UnicodeDictWriter(f, rows().next().keys())
-w.writeheader()
-for row in rows():
-    w.writerow(row)
-f.close()
+    f = open('socrata.csv', 'w')
+    w = UnicodeDictWriter(f, rows().next().keys())
+    w.writeheader()
+    for row in rows():
+        w.writerow(row)
+    f.close()
