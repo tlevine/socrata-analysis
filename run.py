bdd8f3b780a8c3f29ec3187776bc8e7b89d82db4
Thomas Levine <occurrence@thomaslevine.com>
2013-06-26 15:56:31 -0400

    debug

--- run.py
+++ run.py
@@ -5,8 +5,12 @@ import socrata
 
 def rows():
     for portal in os.listdir('data')[1:3]:
-        for viewid in os.listdir(os.path.join('data', portal, 'views'))[:8]:
-            row = socrata.load('data', portal, viewid)
+        for viewid in os.listdir(os.path.join('data', portal, 'views'))[:38]:
+            try:
+                row = socrata.load('data', portal, viewid)
+            except:
+                print portal, viewid
+                raise
             if row != None:
                 yield row
 
