50768f61c589d54f4bc2262abd4e56f15e4341b2
Thomas Levine <occurrence@thomaslevine.com>
2013-06-26 01:36:37 -0700

    more key tests

--- socrata/__init__.py
+++ socrata/__init__.py
@@ -17,6 +17,27 @@ def load(data_dir, portal, viewid):
     return OrderedDict([
         ('portal', portal),
         ('viewid', viewid),
+        ('id', original_data['id']),
+        ('name', original_data['name']),
+        ('attribution', original_data['attribution']),
+        ('averageRating', original_data['averageRating']),
+        ('category', original_data['category']),
+        ('createdAt', original_data['createdAt']),
+        ('description', original_data['description']),
+        ('displayType', original_data['displayType']),
+        ('downloadCount', original_data['downloadCount']),
+        ('numberOfComments', original_data['numberOfComments']),
+        ('oid', original_data['oid']),
+        ('publicationAppendEnabled', original_data['publicationAppendEnabled']),
+        ('publicationDate', original_data['publicationDate']),
+        ('publicationStage', original_data['publicationStage']),
+        ('rowClass', original_data['rowClass']),
+        ('signed', original_data['signed']),
+        ('tableId', original_data['tableId']),
+        ('totalTimesRated', original_data['totalTimesRated']),
+        ('viewCount', original_data['viewCount']),
+        ('viewLastModified', original_data['viewLastModified']),
+        ('viewType', original_data['viewType']),
     ])
 
 def concat_to_matrix(rows):
