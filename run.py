aa9339dcf3ff916fb5c2dca0a81fb4d27d628cf5
Thomas Levine <occurrence@thomaslevine.com>
2013-06-26 15:43:25 -0700

    unicode

--- run.py
+++ run.py
@@ -17,8 +17,38 @@ def rows():
 
 # metadata = socrata.concat_to_array(rows())
 
+
+class UnicodeDictWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, fieldnames, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.DictWiter(self.queue, fieldnames, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
 f = open('socrata.csv', 'w')
-w = csv.DictWriter(f, rows().next().keys())
+w = csv.UnicodeDictWriter(f, rows().next().keys())
 for row in rows():
     w.write(row)
 f.close()
