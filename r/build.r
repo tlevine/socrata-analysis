49d58c778793af38c775fd4cb9b35fc3d1c0f0d9
Thomas Levine <occurrence@thomaslevine.com>
2013-07-03 10:43:39 -0700

    build all the Rmd files

--- r/build.r
+++ r/build.r
@@ -3,4 +3,8 @@ if (!('socrata' %in% ls())) {
   socrata <- read.csv('../socrata.csv', stringsAsFactors = F)
   socrata$createdAt <- as.Date(as.POSIXct(socrata$createdAt, origin = '1970-01-01'))
 }
-knit('01-column_types.Rmd', '01-column_types.md')
+for (Rmd in grep('[.]Rmd$', list.files(), value = T)){
+  md <- sub('Rmd$', 'md', Rmd)
+  knit(Rmd, md)
+  break
+}
