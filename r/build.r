8c8dd703d6d9d8ca6535d67b46dcaba33b3e8540
Thomas Levine <occurrence@thomaslevine.com>
2013-07-03 17:28:44 -0700

    stuff

--- r/build.r
+++ r/build.r
@@ -1,4 +1,8 @@
+library(ggplot2)
+library(plyr)
 library(knitr)
+library(scales)
+
 if (!('socrata' %in% ls())) {
   socrata <- read.csv('../socrata.csv', stringsAsFactors = F)
   socrata$createdAt <- as.Date(as.POSIXct(socrata$createdAt, origin = '1970-01-01'))
