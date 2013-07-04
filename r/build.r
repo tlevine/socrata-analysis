fdcb7106091a15c1f1555fce3cbf3772e76ea2c7
Thomas Levine <occurrence@thomaslevine.com>
2013-07-03 18:39:35 -0700

    turn off the snippets that are breaking

--- r/build.r
+++ r/build.r
@@ -8,6 +8,22 @@ if (!('socrata' %in% ls())) {
   socrata <- read.csv('../socrata.csv', stringsAsFactors = F)
   socrata$createdAt <- as.Date(as.POSIXct(socrata$createdAt, origin = '1970-01-01'))
 }
+
+# Helpers
+listify <- function(datasets) {
+  paste(
+    '<ul>',
+    paste(
+      '<li>',
+        '<a href="https://', datasets$portal, '/-/-/', datasets$id, '">', datasets$name, '</a>',
+        ' (', datasets$downloadCount, ' downloads)',
+      '</li>', sep = ''
+    ),
+    '</ul>',  
+    collapse = '\n'
+  )
+}
+
 for (Rmd in grep('[.]Rmd$', list.files(), value = T)){
   md <- sub('Rmd$', 'md', Rmd)
   knit(Rmd, md)
