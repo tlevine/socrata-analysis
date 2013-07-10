f3622a6294ae12f6be99fdd70e6317523705e8dd
Thomas Levine <occurrence@thomaslevine.com>
2013-07-10 01:48:17 -0700

    move UnicodeDictWriter out of the function

--- python/run.py
+++ python/run.py
@@ -5,6 +5,47 @@ from collections import OrderedDict
 
 import socrata
 
+class UnicodeDictWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, fieldnames, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.DictWriter(self.queue, fieldnames, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    @staticmethod
+    def encode_if_text(value):
+        if isinstance(value, basestring):
+            return value.encode('utf-8')
+        else:
+            return value
+
+    def writerow(self, row):
+        self.writer.writerow(OrderedDict(
+            [(UnicodeDictWriter.encode_if_text(k), UnicodeDictWriter.encode_if_text(v)) for k,v in row.items()]
+        ))
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+    def writeheader(self, *args, **kwargs):
+        return self.writer.writeheader(*args, **kwargs)
+
 def check_json():
     'Delete any files that neither empty nor valid JSON.'
     for portal in os.listdir('data'):
@@ -30,47 +71,6 @@ def build_ndarray():
     return socrata.concat_to_array(rows())
 
 def build_csv():
-    class UnicodeDictWriter:
-        """
-        A CSV writer which will write rows to CSV file "f",
-        which is encoded in the given encoding.
-        """
-
-        def __init__(self, f, fieldnames, dialect=csv.excel, encoding="utf-8", **kwds):
-            # Redirect output to a queue
-            self.queue = cStringIO.StringIO()
-            self.writer = csv.DictWriter(self.queue, fieldnames, dialect=dialect, **kwds)
-            self.stream = f
-            self.encoder = codecs.getincrementalencoder(encoding)()
-
-        @staticmethod
-        def encode_if_text(value):
-            if isinstance(value, basestring):
-                return value.encode('utf-8')
-            else:
-                return value
-
-        def writerow(self, row):
-            self.writer.writerow(OrderedDict(
-                [(UnicodeDictWriter.encode_if_text(k), UnicodeDictWriter.encode_if_text(v)) for k,v in row.items()]
-            ))
-            # Fetch UTF-8 output from the queue ...
-            data = self.queue.getvalue()
-            data = data.decode("utf-8")
-            # ... and reencode it into the target encoding
-            data = self.encoder.encode(data)
-            # write to the target stream
-            self.stream.write(data)
-            # empty queue
-            self.queue.truncate(0)
-
-        def writerows(self, rows):
-            for row in rows:
-                self.writerow(row)
-
-        def writeheader(self, *args, **kwargs):
-            return self.writer.writeheader(*args, **kwargs)
-
     f = open('socrata.csv', 'w')
     w = UnicodeDictWriter(f, rows().next().keys())
     w.writeheader()
