659f0387de0ce0a7efc6445c90667c1a6470e7af
Thomas Levine <occurrence@thomaslevine.com>
2013-07-11 15:05:16 -0700

    yay

--- r/02-geneology/app.js
+++ r/02-geneology/app.js
@@ -2,48 +2,45 @@ function Dataset(canonical_portal, canonical_id, canonical_name) {
   this.portal = canonical_portal
   this.id = canonical_id
   this.name = canonical_name
-  this.portals = {}
+  this.portals = []
   this.url = function(){
     return 'https://' + this.portal + '/-/-/' + this.id
   }
   this.add_derived_dataset = function(derived_portal, derived_id, derived_name) {
-    if (!(derived_portal in this.portals)){
-      this.portals[derived_portal] = new Dataset(derived_portal, canonical_id, canonical_name)
+    if (!(derived_portal in this.portals.map(function(portal){portal.portal}))){
+      dataset = new Dataset(derived_portal, this.id, this.name)
+      this.portals.push(dataset)
     }
-    if (canonical_id === derived_id) {
-      this.portals[derived_portal].name = derived_name
-    } else {
-      this.portals[derived_portal].add_derived_dataset(derived_portal, derived_id, derived_name)
+    for (var i = 0; i < this.portals.length; i++) {
+      if (this.portals[i].portal == this.portal) {
+        if (derived_id === this.id) {
+          this.portals[i].name = derived_name
+        } else {
+          this.portals[i].add_derived_dataset(derived_portal, derived_id, derived_name)
+        }
+        break
+      }
     }
   }
 }
 
-greenbook = new Dataset('explore.data.gov', '5gah-bvex', 'U.S. Overseas Loans and Grants (Greenbook)')
-greenbook.add_derived_dataset('data.hawaii.gov', '5gah-bvex', 'U.S. Overseas Loans and Grants (Greenbook)')
 
-function TodoCtrl($scope) {
-  $scope.todos = [
-    {text:'learn angular', done:true},
-    {text:'build an angular app', done:false}];
+function GeneologyCtrl($scope) {
+  greenbook = new Dataset('explore.data.gov', '5gah-bvex', 'U.S. Overseas Loans and Grants (Greenbook)')
+  greenbook.add_derived_dataset('data.hawaii.gov', '5gah-bvex', 'U.S. Overseas Loans and Grants (Greenbook)')
+
+  $scope.canonical_datasets = [greenbook]
+  $scope.i = 0 // Current dataset
  
-  $scope.addTodo = function() {
-    $scope.todos.push({text:$scope.todoText, done:false});
-    $scope.todoText = '';
-  };
+  $scope.next = function() {
+    $scope.i++
+  }
+  $scope.prev = function() {
+    $scope.i--
+  }
+
+  $scope.d = function() {
+    return $scope.canonical_datasets[$scope.i]
+  }
  
-  $scope.remaining = function() {
-    var count = 0;
-    angular.forEach($scope.todos, function(todo) {
-      count += todo.done ? 0 : 1;
-    });
-    return count;
-  };
- 
-  $scope.archive = function() {
-    var oldTodos = $scope.todos;
-    $scope.todos = [];
-    angular.forEach(oldTodos, function(todo) {
-      if (!todo.done) $scope.todos.push(todo);
-    });
-  };
 }
