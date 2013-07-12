c1041fab6a7571b35651b55de7880781aae0a559
Thomas Levine <occurrence@thomaslevine.com>
2013-07-11 21:56:37 -0700

    ?

--- r/02-geneology/app.js
+++ r/02-geneology/app.js
@@ -2,25 +2,48 @@ function Dataset(params) {
   this.portal = params.portal
   this.id = params.id
   this.name = params.name
+  this.description = params.description
+  this.nrow = params.nrow
+  this.ncol = params.ncol
+  this.createdAt = params.createdAt
+  this.modifyingViewUid = params.modifyingViewUid
+  this.viewCount = params.viewCount
+
   this.portals = []
   this.filtered_views = []
   this.url = function(){
     return 'https://' + this.portal + '/-/-/' + this.id
   }
+
+  this.update = function(new_params){
+    for (param in {"portal":null, "id":null, "name":null, "description":null, "nrow":null, "ncol":null, "createdAt":null, "modifyingViewUid":null, "viewCount":null}){
+      if (typeof(this[param]) !== 'undefined'){
+        this[param] = new_params[param]
+      }
+    }
+    for (list in {"portals":null, "filtered_views":null}) {
+      if (typeof(this[list]) !== 'undefined'){
+        this[list] = this[list].concat(new_params[list])
+      }
+    }
+  }
+
   this.add_derived_dataset = function(derived_params) {
+    var dataset = new Dataset(derived_params)
+
+    // Add the portal if it isn't already there.
     if (this.portals.map(function(portal){return portal.portal}).indexOf(derived_params.portal) === -1){
-      dataset = new Dataset({
-        "portal": derived_params.portal,
-        "id": this.id,
-        "name": this.name
-      })
       this.portals.push(dataset)
     }
+
     for (var i = 0; i < this.portals.length; i++) {
       if (this.portals[i].portal == this.portal) {
-        if (derived_params.id === this.id) {
-          this.portals[i].name = derived_params.name
+        if (derived_params.modifyingViewUid === null) {
+          // Set the portal properties to this one if it is the data portal root (no modifyingViewUid)
+          this.portals[i].update(dataset)
+          console.log(dataset)
         } else {
+          // Add the dataset as a filtered view if it is a filtered view.
           derived_dataset = new Dataset(derived_params)
           this.portals[i].filtered_views.push(derived_dataset)
         }
