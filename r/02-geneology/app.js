f0a6df9485d5b6741e6c0c98eb3703c76df04bc7
Thomas Levine <occurrence@thomaslevine.com>
2013-07-11 21:37:40 -0700

    switch canonical dataset

--- r/02-geneology/app.js
+++ r/02-geneology/app.js
@@ -33,29 +33,29 @@ function Dataset(params) {
 
 function GeneologyCtrl($scope, $http) {
   // Buttons
+  _load = function() {
+    $http.get('geneology/' + $scope.tableIds[$scope.i] + '.json').then(function(res){
+      // Make Dataset objects
+      $scope.canonical_dataset = new Dataset(res.data.source)
+      for (var i = 0; i < res.data.datasets.length; i++) {
+        $scope.canonical_dataset.add_derived_dataset(res.data.datasets[i])
+      }
+    })
+  }
+
   $scope.next = function() {
     $scope.i++
+    _load()
   }
   $scope.prev = function() {
     $scope.i--
+    _load()
   }
 
-  $http.get('geneology/873607.json').then(function(res){
-    // Make Dataset objects
-    canonical_dataset = new Dataset(res.data.source)
-    for (var i = 0; i < res.data.datasets.length; i++) {
-      canonical_dataset.add_derived_dataset(res.data.datasets[i])
-    }
-
-    window.c = canonical_dataset
-
+  $http.get('geneology/index.json').then(function(res){
     // Choose the current dataset
-    $scope.canonical_datasets = [canonical_dataset]
+    $scope.tableIds = res.data
     $scope.i = 0 // Current dataset
-
-    $scope.d = function() {
-      return $scope.canonical_datasets[$scope.i]
-    }
- 
+    _load()
   })
 }
