cf406daa891312ac1365e8c0cc9e0caf7d517509
Thomas Levine <occurrence@thomaslevine.com>
2013-07-11 20:44:36 -0700

    blah

--- r/02-geneology/app.js
+++ r/02-geneology/app.js
@@ -1,22 +1,22 @@
-function Dataset(canonical_portal, canonical_id, canonical_name) {
-  this.portal = canonical_portal
-  this.id = canonical_id
-  this.name = canonical_name
+function Dataset(params) {
+  this.portal = params.portal
+  this.id = params.id
+  this.name = params.name
   this.portals = []
   this.url = function(){
     return 'https://' + this.portal + '/-/-/' + this.id
   }
-  this.add_derived_dataset = function(derived_portal, derived_id, derived_name) {
-    if (!(derived_portal in this.portals.map(function(portal){portal.portal}))){
-      dataset = new Dataset(derived_portal, this.id, this.name)
+  this.add_derived_dataset = function(derived_params) {
+    if (!(derived_params.portal in this.portals.map(function(portal){portal.portal}))){
+      dataset = new Dataset(derived_params.portal, this.id, this.name)
       this.portals.push(dataset)
     }
     for (var i = 0; i < this.portals.length; i++) {
       if (this.portals[i].portal == this.portal) {
-        if (derived_id === this.id) {
-          this.portals[i].name = derived_name
+        if (derived_params.id === this.id) {
+          this.portals[i].name = derived_params.name
         } else {
-          this.portals[i].add_derived_dataset(derived_portal, derived_id, derived_name)
+          this.portals[i].add_derived_dataset(derived_params.portal, derived_params.id, derived_params.name)
         }
         break
       }
@@ -25,11 +25,18 @@ function Dataset(canonical_portal, canonical_id, canonical_name) {
 }
 
 
-function GeneologyCtrl($scope) {
-  greenbook = new Dataset('explore.data.gov', '5gah-bvex', 'U.S. Overseas Loans and Grants (Greenbook)')
-  greenbook.add_derived_dataset('data.hawaii.gov', '5gah-bvex', 'U.S. Overseas Loans and Grants (Greenbook)')
+function GeneologyCtrl($scope, $http) {
+  var canonical_dataset = $http.get('geneology/873607.json').then(function(res){
+    canonical_dataset = new Dataset(res.data.source)
+    res.data.datasets.map(function(dataset){
+      canonical_dataset.add_derived_dataset(dataset)
+    })
+    console.log(canonical_dataset)
+    return canonical_dataset
+  })
+  window.c = canonical_dataset
 
-  $scope.canonical_datasets = [greenbook]
+  $scope.canonical_datasets = [canonical_dataset]
   $scope.i = 0 // Current dataset
  
   $scope.next = function() {
