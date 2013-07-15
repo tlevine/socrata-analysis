3e311b27c2ebcf8390875deadd6c361898befb8d
Thomas Levine <occurrence@thomaslevine.com>
2013-07-15 16:15:23 -0700

    sorting

--- r/02-geneology/app.js
+++ r/02-geneology/app.js
@@ -1,10 +1,23 @@
+// http://stackoverflow.com/questions/979256/sorting-an-array-of-javascript-objects
+var sortBy = function(field, reverse, primer){
+   var key = function (x) {return primer ? primer(x[field]) : x[field]};
+   return function (a,b) {
+       var A = key(a), B = key(b);
+       return ((A < B) ? -1 : (A > B) ? +1 : 0) * [-1,1][+!!reverse];                  
+   }
+}
+
 function GeneologyCtrl($scope, $http) {
   // Buttons
   _load = function() {
     $http.get('geneology/' + $scope.tableIds[$scope.i] + '.json').then(function(res){
       $scope.table = res.data
+      $scope.table.datasets = $scope.table.datasets.sort(sortBy($scope.sortField, $scope.sortReverse))
     })
   }
+
+  $scope.sortField = 'downloadCount'
+  $scope.sortReverse = false
 
   $scope.next = function() {
     $scope.i++
