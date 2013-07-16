a70cc9fbcf501c47da9f151d4ff0b0c2a3c4cd23
Thomas Levine <occurrence@thomaslevine.com>
2013-07-15 19:17:58 -0700

    try angularstrap

--- r/02-geneology/app.js
+++ r/02-geneology/app.js
@@ -22,42 +22,34 @@ var MONTHS = [
   'December'
 ]
 
-angular.module('geneology', ['angular-table'])
+angular.module('geneology', ['angular-table', '$strap.directives'])
   .controller('GeneologyCtrl', ['$scope', '$http', function($scope, $http) {
   // Buttons
-  _load = function() {
-    $http.get('geneology/' + $scope.tableIds[$scope.i][0] + '.json').then(function(res){
+  _load = function(i) {
+    $http.get('geneology/' + $scope.tableIds[i][0] + '.json').then(function(res){
       $scope.table = res.data
       $scope.table.datasets = $scope.table.datasets.map(function(dataset) {
-      $scope.mainPortal = $scope.tableIds[$scope.i][1]
+      $scope.mainPortal = $scope.tableIds[i][1]
         var d = new Date()
         d.setTime(1000 * dataset.createdAt)
         dataset.prettyDate = MONTHS[d.getMonth()] + ' ' + d.getDate() + ', ' + d.getFullYear()
         dataset.ncell = dataset.nrow * dataset.ncol
         dataset.ncopies = dataset.portals.length
-        dataset.mainPortal = $scope.tableIds[$scope.i][1] // Redundant but it works
+        dataset.mainPortal = $scope.tableIds[i][1] // Redundant but it works
         return dataset
       })
     })
   }
 
-  $scope.handleRowSelection = function(row) {
-    row.selected = true
-  }
-
-  $scope.next = function() {
-    $scope.i++
-    _load()
-  }
-  $scope.prev = function() {
-    $scope.i--
-    _load()
-  }
-
   $http.get('geneology/index.json').then(function(res){
     // Choose the current dataset
     $scope.tableIds = res.data
-    $scope.i = 2 // Current dataset
-    _load()
+    $scope.dropdown = $scope.tableIds.map(function(tableId) {
+      return {
+        "text": tableId[2],
+        "href": "_load(" + tableId[0] + ")"
+      }
+    })
+    _load(2)
   })
 }])
