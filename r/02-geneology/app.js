b0e25eaa9417c91d5a4d37308f69583a1c00836c
Thomas Levine <occurrence@thomaslevine.com>
2013-07-16 17:15:45 -0700

    one json file and everything works

--- r/02-geneology/app.js
+++ r/02-geneology/app.js
@@ -1,12 +1,3 @@
-// http://stackoverflow.com/questions/979256/sorting-an-array-of-javascript-objects
-var sortBy = function(field, reverse, primer){
-   var key = function (x) {return primer ? primer(x[field]) : x[field]};
-   return function (a,b) {
-       var A = key(a), B = key(b);
-       return ((A < B) ? -1 : (A > B) ? +1 : 0) * [-1,1][+!!reverse];                  
-   }
-}
-
 var MONTHS = [
   'January',
   'February',
@@ -25,35 +16,17 @@ var MONTHS = [
 angular.module('geneology', ['angular-table'])
   .controller('GeneologyCtrl', ['$scope', '$http', function($scope, $http) {
   // Buttons
-  _load = function(i) {
-    $http.get('geneology/' + $scope.tableIds[i][0] + '.json').then(function(res){
-      $scope.table = res.data
-      $scope.table.datasets = $scope.table.datasets.map(function(dataset) {
-      $scope.mainPortal = $scope.tableIds[i][1]
+  $http.get('geneology.json').then(function(res){
+    $scope.tables = res.data.map(function(table){
+      table.datasets = table.datasets.map(function(dataset) {
         var d = new Date()
         d.setTime(1000 * dataset.createdAt)
         dataset.prettyDate = MONTHS[d.getMonth()] + ' ' + d.getDate() + ', ' + d.getFullYear()
         dataset.ncell = dataset.nrow * dataset.ncol
         dataset.ncopies = dataset.portals.length
-        dataset.mainPortal = $scope.tableIds[i][1] // Redundant but it works
         return dataset
       })
+      return table
     })
-  }
-
-  $http.get('geneology/index.json').then(function(res){
-    // Choose the current dataset
-    $scope.tableIds = res.data
-    $scope.tableIdIndices = []
-    for (var i = 0; i < $scope.tableIds.length; i++) {
-      $scope.tableIdIndices.push(i)
-    }
-    $scope.dropdown = $scope.tableIds.map(function(tableId) {
-      return {
-        "text": tableId[2],
-        "href": "_load(" + tableId[0] + ")"
-      }
-    })
-    _load(2)
   })
 }])
