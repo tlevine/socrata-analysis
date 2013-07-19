90c902a1e6e13743bd5852262c75fad4043409a8
Thomas Levine <occurrence@thomaslevine.com>
2013-07-19 01:41:47 -0700

    plots

--- r/02-geneology.R
+++ r/02-geneology.R
@@ -1,3 +1,6 @@
+library(ggplot2)
+library(scales)
+
 d <- t(matrix(c(
   71042, 48786, 3505, 732,
   5466, 4145, 1403, 537,
@@ -10,7 +13,6 @@ d <- t(matrix(c(
   281339, 7268, 10741, 633,
   1455933, 1351374, 26485, 10398
 ), 4))
-
 dataset.names <- c(
   'TIF Projection Reports',
   'FEC Contributions',
@@ -23,11 +25,29 @@ dataset.names <- c(
   'IDA Statement of Credits',
   'White House Visitor'
 )
-
 top.datasets <- data.frame(
   name = rep(dataset.names, 4),
   variable = factor(rep(c('Hits','Downloads'), each = 20)),
-  scope = factor(rep(c('Family','Source'), each = 10)),
+  scope = factor(rep(c('Whole family','Source only'), each = 10)),
   value = c(d[,1], d[,2], d[,3], d[,4])
 )
+rm(d)
+rm(dataset.names)
 
+plot.family <- function(variable.name) {
+  p <- ggplot(subset(top.datasets, variable == variable.name)) +
+    aes(y = name, x = value, color = scope) +
+    scale_x_log10('Value', labels = comma, breaks = 10^c(2:9)) +
+    scale_y_discrete('Dataset') +
+    ggtitle(paste(variable.name, 'by dataset family')) +
+    geom_line(aes(group = name), color = 'black') +
+    geom_point(aes(size = 100))
+  png(paste0('02-geneology/', variable.name, '.png'),
+    width = 840, height = 610, res = 100)
+  print(p)
+  dev.off()
+}
+
+for (v in levels(top.datasets$variable)) {
+  plot.family(v)
+}
