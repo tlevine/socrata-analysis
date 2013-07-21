7a8dc598e587b33879f4c20775086359a3d9460c
Thomas Levine <occurrence@thomaslevine.com>
2013-07-21 15:09:59 -0700

    graph tests

--- numbers/graph.py
+++ numbers/graph.py
@@ -1,34 +1,59 @@
+from collections import Counter
+
 class Graph:
     def __init__(self):
-        self.users = {
-            'tables': {},
-            'views': {},
-            'view_types': {},
-        }
-        self.tables = {
-            'users': {},
-            'views': {},
-            'view_types': {},
-        }
-        self.views = {
-            'tables': {},
-            'users': {},
-            'view_types': {},
-        }
-        self.view_types = {
-            'tables': {},
-            'users': {},
-            'views': {},
-        }
+        pass
 
     def add(self, view):
         "Given a view's metadata, add the necessary things to the graph."
         view.get('displayType')
         view['owner']['id']
         view['tableId']
+        view['id']
 
     def _add_table(self, tableId, tableAuthor):
         'Add a table (a family of views)'
 
     def _add_user(self, owner):
         'Add a user (taken from the `owner` field)'
+
+class NodeFactories:
+    @staticmethod
+    def _user(user_id):
+        return {
+            user_id: {
+                'tables': Counter(),
+                'views': Counter(),
+                'view_types': Counter(),
+            }
+        }
+
+    @staticmethod
+    def _view(view_id):
+        return {
+            view_id: {
+                'users': Counter(),
+                'views': Counter(),
+                'view_types': Counter(),
+            }
+        }
+
+    @staticmethod
+    def _table(table_id):
+        return {
+            table_id: {
+                'tables': Counter(),
+                'users': Counter(),
+                'view_types': Counter(),
+            }
+        }
+
+    @staticmethod
+    def _view_type(view_type):
+        return {
+            view_type: {
+                'tables': Counter(),
+                'users': Counter(),
+                'views': Counter(),
+            }
+        }
---
+++ numbers/test_graph.py
@@ -0,0 +1,65 @@
+import nose.tools as n
+import unittest
+
+from collections import Counter
+from graph import Graph, NodeFactories
+
+class TestGraph(unittest.TestCase):
+    def setUp(self):
+        self.graph = Graph()
+
+    def test_add_user(self):
+        owner = {
+            "id" : "xbqz-g2u5",
+            "displayName" : "Matt Smith",
+            "emailUnsubscribed" : False,
+            "privacyControl" : "login",
+            "profileLastModified" : 1300299847,
+            "roleName" : "administrator",
+            "screenName" : "Matt Smith",
+            "rights" : [ "create_datasets", "edit_others_datasets", "edit_sdp", "edit_site_theme", "moderate_comments", "manage_users", "chown_datasets", "edit_nominations", "approve_nominations", "feature_items", "federations", "manage_stories", "manage_approval", "change_configurations", "view_domain", "view_others_datasets", "edit_pages", "create_pages" ]
+          }
+        self.graph._add_user(owner)
+#       n.assert_dict_equal(self.graph.users, Counter({owner['id']: 1}))
+
+class TestNodeFactories(unittest.TestCase):
+    def test_user(self):
+        observed = NodeFactories._user('abcd-efhg')
+        expected = {
+            'abcd-efhg': {
+                'tables': Counter(),
+                'views': Counter(),
+                'view_types': Counter(),
+            }
+        }
+        n.assert_dict_equal(observed, expected)
+
+    def test_view(self):
+        observed = NodeFactories._view('abcd-efhg')
+        expected = {
+            'abcd-efgh': {
+                'users': Counter(),
+                'views': Counter(),
+                'view_types': Counter(),
+            }
+        }
+
+    def test_table(self):
+        observed = NodeFactories._table('abcd-efhg')
+        expected = {
+            'abcd-efgh': {
+                'tables': Counter(),
+                'users': Counter(),
+                'view_types': Counter(),
+            }
+        }
+
+    def test_view_type(self):
+        observed = NodeFactories._view_type('abcd-efhg')
+        expected = {
+            'abcd-efgh': {
+                'tables': Counter(),
+                'users': Counter(),
+                'views': Counter(),
+            }
+        }
