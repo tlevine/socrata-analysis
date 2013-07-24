04b13467ece7d5dfb72d65387cbe52b9d67f5927
Thomas Levine <occurrence@thomaslevine.com>
2013-07-23 23:55:25 -0700

    user intensity

--- words/build.r
+++ words/build.r
@@ -10,6 +10,8 @@ if (!('socrata' %in% ls())) {
   socrata <- read.csv('../socrata.csv', stringsAsFactors = F)
   socrata$createdAt <- as.Date(as.POSIXct(socrata$createdAt, origin = '1970-01-01'))
   socrata$publicationDate <- as.Date(as.POSIXct(socrata$publicationDate, origin = '1970-01-01'))
+  socrata$date <- socrata$createdAt
+  socrata[is.na(socrata$date),'date'] <- socrata[is.na(socrata$createdAt),'publicationDate']
 }
 
 if (!('users' %in% ls())) {
